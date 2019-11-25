c91c7d4a1d203d0f0619dd7fc667f2d866ea8f23
Przemysław Solan <przemek.solan@mycit.ie>
2019-11-25 19:24:21 +0000

    Process updated

--- src/routes/process.svelte
+++ src/routes/process.svelte
@@ -1,12 +1,25 @@
 <svelte:head>
-	<title>Case Studies - Przemysław Solan</title>
+	<title>Process - Przemysław Solan</title>
 </svelte:head>
 
-<h2>Case Studies</h2>
+<h2>Process</h2>
 
-<section class="" role="store">
-    <div>
-        <a href="https://dazzling-swirles-a8a080.netlify.com/">Cork Book Fair</a><div class="text-muted sm-p">Front End Frameworks</div>
-    </div>
-    <p>An international book fair organized by the Enterprise Ireland and Higher Education Ireland, it will be held annually at the Pairc Ui Chaoimh and the Cork City Hall, selling and exhibiting books, printed matter, stationary, printing, compact discs and other multimedia publishing.</p>
+<section>
+    <div class="text-muted">Ruby on Rails</div>
+    <p>Make use of the perfect solution when the speed of development is crucial to your success.</p>
+</section>
+
+<section>
+    <div class="text-muted">React.js</div>
+    <p>Implement beautiful and responsive user interfaces that are highly-interactive and give users a native-like experience.</p>
+</section>
+
+<section>
+    <div class="text-muted">Node.js</div>
+    <p>Build performant web applications based on Javascript – one language for full-stack development.</p>
+</section>
+
+<section>
+    <div class="text-muted">Python</div>
+    <p>Python is currently one of the fastest growing languages to get your job done on time.</p>
 </section>
